c07ba4a23ba81dd20da56ea495d652fc137f824e
samuel-wigglesworth <c.samuelwigglesworth@gmail.com>
2026-09-23 13:16:30 +0530

    Delete Resort-Booking-System-Project-main directory

--- Resort-Booking-System-Project-main/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Room
-
-def room_list(request):
-    rooms = Room.objects.filter(is_available=True)
-    return render(request, 'rooms/room_list.html', {'rooms': rooms})
